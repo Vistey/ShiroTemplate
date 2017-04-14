6f4b63cca583c3ee5a55ff6cd2fc67c647894253
vistey <512272538@qq.com>
2017-04-14 11:10:28 +0800

    test

--- src/main/java/com/controller/LookupTest/HjlCarrier.java
+++ src/main/java/com/controller/LookupTest/HjlCarrier.java
@@ -1,11 +1,16 @@
 package com.controller.LookupTest;
 
 import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.stereotype.Controller;
 
 
 /**
- *
+ * @controller 注释可以省略bean的配置
+ * @lookup 注释代替xml文件中的lookup-method标签
+ * 只有需要被装配的资源需要用到@Resource来注释
+ * @Scope("prototype") 注释代替bean中scope=“prototype”属性
  */
+@Controller
 public abstract class HjlCarrier {
 
   private WhlCarrier whlCarrier;
--- src/main/java/com/controller/LookupTest/WhlCarrier.java
+++ src/main/java/com/controller/LookupTest/WhlCarrier.java
@@ -1,7 +1,12 @@
 package com.controller.LookupTest;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
 /**
  *
  */
+@Scope("prototype")
+@Service
 public class WhlCarrier {
 }
